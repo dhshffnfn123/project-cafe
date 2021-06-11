71dbe3d311d9b5a6c70f4de9ead2ed2675f72bcd
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-11 20:12:07 +0900

    default frame 생성

---
+++ Cafe-POS-System/src/swing/frame/DefaultFrame.java
@@ -0,0 +1,14 @@
+package swing.frame;
+
+import javax.swing.JFrame;
+
+public class DefaultFrame extends JFrame {
+	
+	public DefaultFrame() {
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1500, 1000);
+		setVisible(true);
+	}
+	
+}
