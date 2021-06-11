7f57c096e1fe1c12091b939197bfb71ae0619f84
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-11 18:16:18 +0900

    RTD model 변수명 수정

--- Cafe-POS-System/src/jdbc/model/Rtd.java
+++ Cafe-POS-System/src/jdbc/model/Rtd.java
@@ -2,32 +2,32 @@ package jdbc.model;
 
 public class Rtd {
 	
-	private int RTD_id ;
-	private String RTD_name;
-	private int RTD_price;
+	private int rtd_id;
+	private String rtd_name;
+	private int rtd_price;
 	
-	public int getRTD_id() {
-		return RTD_id;
+	public int getRtd_id() {
+		return rtd_id;
 	}
 	
-	public void setRTD_id(int rTD_id) {
-		RTD_id = rTD_id;
+	public void setRtd_id(int rtd_id) {
+		this.rtd_id = rtd_id;
 	}
 	
-	public String getRTD_name() {
-		return RTD_name;
+	public String getRtd_name() {
+		return rtd_name;
 	}
 	
-	public void setRTD_name(String rTD_name) {
-		RTD_name = rTD_name;
+	public void setRtd_name(String rtd_name) {
+		this.rtd_name = rtd_name;
 	}
 	
-	public int getRTD_price() {
-		return RTD_price;
+	public int getRtd_price() {
+		return rtd_price;
 	}
 	
-	public void setRTD_price(int rTD_price) {
-		RTD_price = rTD_price;
+	public void setRtd_price(int rtd_price) {
+		this.rtd_price = rtd_price;
 	}
-
+	
 }
