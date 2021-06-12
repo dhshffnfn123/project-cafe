999c4af0aafcd743cad3c7c662da8ec1c689b15f
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-12 22:31:08 +0900

    직원 정보 삭제 버튼 쿼리문, 세팅 변수 변경

--- Cafe-POS-System/src/action/EmployeeInfoDelButton.java
+++ Cafe-POS-System/src/action/EmployeeInfoDelButton.java
@@ -14,16 +14,16 @@ import jdbc.hikari.HikariCP;
 
 public class EmployeeInfoDelButton implements ActionListener {
 
-	private String sql = "DELETE FROM employees_table WHERE employee_name = ? AND employee_pw = ? AND employee_grade = ?";
+	private String sql = "DELETE FROM employees_table WHERE employee_id = ? AND employee_name = ? AND employee_pw = ? AND employee_grade = ?";
+	private String employee_id;
 	private String employee_name;
 	private String employee_pw;
 	private String employee_grade;
 	private ArrayList<TextField> fields = new ArrayList<>();
 	private JComboBox<String> grade_box;
 	
-	public EmployeeInfoDelButton(ArrayList<TextField> fields, JComboBox<String> grade_box) {
+	public EmployeeInfoDelButton(ArrayList<TextField> fields) {
 		this.fields = fields;
-		this.grade_box = grade_box;
 	}
 	
 	@Override
@@ -32,13 +32,15 @@ public class EmployeeInfoDelButton implements ActionListener {
 				Connection conn = HikariCP.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 		) {
-			this.employee_name = fields.get(0).getText();
-			this.employee_pw = fields.get(1).getText();
-			this.employee_grade = (String)grade_box.getSelectedItem();
+			this.employee_id = fields.get(0).getText();
+			this.employee_name = fields.get(1).getText();
+			this.employee_pw = fields.get(2).getText();
+			this.employee_grade = fields.get(3).getText();
 			
-			pstmt.setString(1, employee_name);
-			pstmt.setString(2, employee_pw);
-			pstmt.setString(3, employee_grade);
+			pstmt.setString(1, employee_id);
+			pstmt.setString(2, employee_name);
+			pstmt.setString(3, employee_pw);
+			pstmt.setString(4, employee_grade);
 			
 			pstmt.executeQuery();
 		} catch (SQLException e2) {
