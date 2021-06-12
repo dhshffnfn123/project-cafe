0eff21dbda6fa3b51643030e24762ace2a0a0b13
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-12 22:28:06 +0900

    직원 정보 수정 버튼 쿼리, 세팅 변수 변경

--- Cafe-POS-System/src/action/EmployeeInfoUpdateButton.java
+++ Cafe-POS-System/src/action/EmployeeInfoUpdateButton.java
@@ -14,7 +14,8 @@ import jdbc.hikari.HikariCP;
 
 public class EmployeeInfoUpdateButton implements ActionListener {
 
-	private String sql = "UPDATE employees_table SET employee_name = ? AND employee_pw = ? AND employee_grade = ?";
+	private String sql = "UPDATE employees_table SET empoloyee_id = ?, employee_name = ? WHERE employee_pw = ? AND employee_grade = ?";
+	private String employee_id;
 	private String employee_name;
 	private String employee_pw;
 	private String employee_grade;
@@ -32,13 +33,15 @@ public class EmployeeInfoUpdateButton implements ActionListener {
 				Connection conn = HikariCP.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 		) {
-			this.employee_name = fields.get(0).getText();
-			this.employee_pw = fields.get(1).getText();
+			this.employee_id = fields.get(0).getText();
+			this.employee_name = fields.get(1).getText();
+			this.employee_pw = fields.get(2).getText();
 			this.employee_grade = (String)grade_box.getSelectedItem();
 			
-			pstmt.setString(1, employee_name);
-			pstmt.setString(2, employee_pw);
-			pstmt.setString(3, employee_grade);
+			pstmt.setString(1, employee_id);
+			pstmt.setString(2, employee_name);
+			pstmt.setString(3, employee_pw);
+			pstmt.setString(4, employee_grade);
 			
 			pstmt.executeQuery();
 		} catch (SQLException e2) {
