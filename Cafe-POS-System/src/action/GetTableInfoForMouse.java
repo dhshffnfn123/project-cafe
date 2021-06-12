07bcafe5a554d83f882be649cdf4f80ccfc72550
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-12 22:31:49 +0900

    마우스 액션 각 탭 마다 기능 다르게 설정

--- Cafe-POS-System/src/action/GetTableInfoForMouse.java
+++ Cafe-POS-System/src/action/GetTableInfoForMouse.java
@@ -1,20 +1,26 @@
 package action;
 
+import java.awt.Font;
+import java.awt.TextField;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
+import javax.swing.JComboBox;
 import javax.swing.JTable;
 
 public class GetTableInfoForMouse implements MouseListener {
 
 	private JTable table;
+	private ArrayList<TextField> fields;
 	private String employee_id;
 	private String employee_name;
 	private String employee_password;
 	private String employee_grade;
 	
-	public GetTableInfoForMouse(JTable table) {
+	public GetTableInfoForMouse(JTable table, ArrayList<TextField> fields) {
 		this.table = table;
+		this.fields = fields;
 	}
 	
 	@Override
@@ -24,6 +30,25 @@ public class GetTableInfoForMouse implements MouseListener {
 		employee_name = (String)table.getModel().getValueAt(row, 1);
 		employee_password = (String)table.getModel().getValueAt(row, 2);
 		employee_grade = (String)table.getModel().getValueAt(row, 3);
+		
+		if (fields.size() == 2) {
+			fields.get(0).setText(employee_name);
+			fields.get(0).setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 30));
+			fields.get(0).update(null);
+			fields.get(1).setText(employee_password);
+			fields.get(1).setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 30));
+		}
+		// ¼öÁ¤ ÅÇÀÎ °æ¿ì, »èÁ¦ ÅÇÀÎ °æ¿ì ±¸ºÐ
+		if (fields.size() == 3) {
+			fields.get(0).setText(employee_id);
+			fields.get(1).setText(employee_name);
+			fields.get(2).setText(employee_password);
+		} else if (fields.size() == 4) {
+			fields.get(0).setText(employee_id);
+			fields.get(1).setText(employee_name);
+			fields.get(2).setText(employee_password);
+			fields.get(3).setText(employee_grade);
+		}
 	}
 
 	@Override
