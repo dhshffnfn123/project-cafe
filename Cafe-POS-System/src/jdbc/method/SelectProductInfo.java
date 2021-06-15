42666dc7cbbc9d1b10bda153ea96f3e99fc4306b
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-15 19:01:59 +0900

    영수증 뷰에 사용되는 product_table의 값을 가져오는 메서드 구현

--- Cafe-POS-System/src/jdbc/method/SelectProductInfo.java
+++ Cafe-POS-System/src/jdbc/method/SelectProductInfo.java
@@ -10,21 +10,24 @@ import jdbc.hikari.HikariCP;
 public class SelectProductInfo {
 	private String sql = "SELECT p.product_name, r.menu_count, p.product_price FROM product_table p INNER JOIN receipt_table r USING(product_id)";
 	private int sum;
+	private int productIndex;
 	public int getSum() {
 		return sum;
 	}
-	public SelectProductInfo() {
+	
+	public SelectProductInfo(int productIndex) {
 		try (
 				Connection conn = HikariCP.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(sql);
+				PreparedStatement pstmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, 
+			             ResultSet.CONCUR_UPDATABLE);
 				ResultSet rs = pstmt.executeQuery();
 		) {
-			sum = 0;
-			while (rs.next()) {
+			rs.relative(productIndex);
+			if (rs.next()) {				
 				System.out.printf("¦¢%-20s\t%5d\t%4d\t%8d¦¢\n", rs.getString(1), rs.getInt(3), rs.getInt(2), rs.getInt(2) * rs.getInt(3));
 				sum += rs.getInt(2) * rs.getInt(3);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
