05ff673b0d81dc60827daff4dd71d9e6fab64407
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-15 19:02:07 +0900

    영수증 뷰에 사용되는 rtd_table의 값을 가져오는 메서드 구현

--- Cafe-POS-System/src/jdbc/method/SelectRtdInfo.java
+++ Cafe-POS-System/src/jdbc/method/SelectRtdInfo.java
@@ -8,19 +8,22 @@ import java.sql.SQLException;
 import jdbc.hikari.HikariCP;
 
 public class SelectRtdInfo {
-	private String sql = "SELECT rtd_name, t.rtd_price, r.menu_count FROM rtd_table t INNER JOIN receipt_table r USING(rtd_id)";
+	private String sql = "SELECT rtd_name, rtd_price, menu_count FROM rtd_table t INNER JOIN receipt_table r USING(rtd_id)";
 	private int sum;
+	private int rtdIndex;
 	public int getSum() {
 		return sum;
 	}
-	public SelectRtdInfo() {
+	
+	public SelectRtdInfo(int rtdIndex) {
 		try (
 				Connection conn = HikariCP.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(sql);
+				PreparedStatement pstmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, 
+			             ResultSet.CONCUR_UPDATABLE);
 				ResultSet rs = pstmt.executeQuery();
 		) {
-			sum = 0;
-			while (rs.next()) {
+			rs.relative(rtdIndex);
+			if (rs.next()) {
 				System.out.printf("¦¢%-20s\t%5d\t%4d\t%8d¦¢\n", rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getInt(2) * rs.getInt(3));
 				sum += rs.getInt(2) * rs.getInt(3);
 			}
