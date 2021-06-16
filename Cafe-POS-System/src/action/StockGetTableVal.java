c5c4b9cf1788a483b0655f0634e6b6be90916102
kes0421 <kea0421@naver.com>
2021-06-16 14:47:23 +0900

    재고 테이블에서 정보 얻어오는 기능 구현

---
+++ Cafe-POS-System/src/action/StockGetTableVal.java
@@ -0,0 +1,54 @@
+package action;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+
+public class StockGetTableVal implements MouseListener {
+	
+	private JTable table;
+	private int id, count;
+	private String name;
+
+	public StockGetTableVal(JTable table) {
+		this.table = table;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+			int row = table.getSelectedRow(); // 선택한 셀의 행 번호 계산
+			TableModel model = table.getModel();
+
+			id = (int) model.getValueAt(row, 0);
+			name = (String) model.getValueAt(row, 1);
+			count = (int) model.getValueAt(row, 2);
+
+			System.out.println("ID : " + id);
+			System.out.println("NAME : " + name);
+			System.out.println("COUNT : " + count);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+}
