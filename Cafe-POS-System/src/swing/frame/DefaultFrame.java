e58cabb0c400814af286b5883ae036b7c9b8d2d3
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-16 17:13:34 +0900

    default frame 아이콘 이미지 변경. image 저장할 폴더 생성

--- Cafe-POS-System/src/swing/frame/DefaultFrame.java
+++ Cafe-POS-System/src/swing/frame/DefaultFrame.java
@@ -1,10 +1,20 @@
 package swing.frame;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class DefaultFrame extends JFrame {
+
+	private Toolkit kit;
+	private Image image;
 	
 	public DefaultFrame() {
+		kit = Toolkit.getDefaultToolkit();
+		image = kit.getImage("./Image/Icon.png");
+		setIconImage(image);
+		
 		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(1500, 1000);
