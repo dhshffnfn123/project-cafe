05c1303550c78f579f185f099b9e3af00d6a8ce7
kes0421 <kea0421@naver.com>
2021-06-16 14:51:04 +0900

    로그인 시 회원정보 등록 되어있는지 불러오는 기능 구현

---
+++ Cafe-POS-System/src/jdbc/method/LoginComboxData.java
@@ -0,0 +1,39 @@
+package jdbc.method;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JComboBox;
+
+import jdbc.hikari.HikariCP;
+
+public class LoginComboxData {
+	private String sql = "SELECT * FROM employees_table";
+	private JComboBox combox;
+	
+	public JComboBox getComboBox() {
+		ComboBox_addData();
+		return combox;
+	}
+	
+	private void ComboBox_addData() {
+		combox = new JComboBox();
+		combox.addItem("ID를 선택하세요");
+		
+		try (
+				Connection conn = HikariCP.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery();
+		) {
+			while (rs.next()) {
+				combox.addItem(rs.getString(2));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+}
