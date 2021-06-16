a08654d469b940c46dba67c182c7139cece31ff6
kes0421 <kea0421@naver.com>
2021-06-16 14:45:18 +0900

    재고 삭제 버튼 기능 구현

---
+++ Cafe-POS-System/src/action/StockCancelBtn.java
@@ -0,0 +1,20 @@
+package action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class StockCancelBtn implements ActionListener{
+	private JFrame frame;
+	
+	public StockCancelBtn(JFrame frame) {
+		this.frame = frame;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		frame.dispose();
+	}
+
+}
