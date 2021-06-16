0986229b307582f726960855569f6fac712aa571
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-16 10:19:38 +0900

    Receipt 정보를 Print 후 DropSequence 를 진행해주는 Method 구현

---
+++ Cafe-POS-System/src/jdbc/method/DropSequenceReceiptInfo.java
@@ -0,0 +1,24 @@
+package jdbc.method;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jdbc.hikari.HikariCP;
+
+// 영수증의 시퀀스 제거 후 바로 생성
+public class DropSequenceReceiptInfo {
+	private String sql = "DROP SEQUENCE receipt_id_seq";
+
+	public DropSequenceReceiptInfo() {
+		try (
+				Connection conn = HikariCP.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				) {
+			pstmt.executeUpdate();
+			new CreateSequenceReceiptInfo();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
