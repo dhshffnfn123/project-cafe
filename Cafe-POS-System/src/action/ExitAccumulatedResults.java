a766f34aff9376dfb1fca290ae9b6ea195aa3c9a
sinseungmi <tlstmdal3@naver.com>
2021-06-17 13:57:02 +0900

    결과창 확인버튼 구현

---
+++ Cafe-POS-System/src/action/ExitAccumulatedResults.java
@@ -0,0 +1,15 @@
+package action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ExitAccumulatedResults implements ActionListener{
+
+	//확인 종료 액션
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+		
+	}
+
+}
