68da2e580d7730c377e43ee066fefebe342b11ca
sinseungmi <tlstmdal3@naver.com>
2021-06-17 13:55:07 +0900

    적립버튼 클릭시 전화번호 일치하는지 확인하고 번호에 하이픈 추가

---
+++ Cafe-POS-System/src/action/AccumulatedFundTextFiel.java
@@ -0,0 +1,29 @@
+package action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import swing.frame.RoundJTextField;
+
+
+//텍스트필드 클릭시 기존에 있던 "전화번호를 입력하세요" 텍스트를 사라지게 만드는 클래스
+public class AccumulatedFundTextFiel extends MouseAdapter {
+	private RoundJTextField textField;
+	
+	public AccumulatedFundTextFiel(RoundJTextField textField) {
+		this.textField = textField;
+	}
+	
+	public void mouseClicked(MouseEvent e) { 
+		String fieldDefaultText = "전화번호를 입력하세요";
+		if (textField.getText().equals(fieldDefaultText)) {
+			textField.setText(null); 
+		}else {
+			return;
+		}
+		
+	}
+
+}
