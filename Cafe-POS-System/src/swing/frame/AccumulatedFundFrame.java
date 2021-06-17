bf5219b8e52bd9ce0cc3b1029a1f3dd51abad2d6
sinseungmi <tlstmdal3@naver.com>
2021-06-17 13:52:41 +0900

    적립 프레임 구현 완료

--- Cafe-POS-System/src/swing/frame/AccumulatedFundFrame.java
+++ Cafe-POS-System/src/swing/frame/AccumulatedFundFrame.java
@@ -8,19 +8,22 @@ import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.ColorConvertOp;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
-import action.AccumulatedTextFiel;
-import action.AccumulatedButton;
+import action.AccumulatedFundButton;
+import action.AccumulatedFundTextFiel;
 
 
 public class AccumulatedFundFrame extends DefaultFrame{
@@ -33,29 +36,42 @@ public class AccumulatedFundFrame extends DefaultFrame{
 	
 	public AccumulatedFundFrame() {
 	
+		//(프레임)====================================================================
+		
 		//기본 프레임 설정
 		setTitle("적립 화면");
-		setSize(450,610);
+		setSize(420,580);
 		setLocation(500,100);
 		setResizable(false);
-		getContentPane().setBackground(new Color(245, 235, 208));
+		getContentPane().setBackground(new Color(243, 235, 207));
 		
 		//입력 필드 설정, 글씨 설정
 		Font font = new Font("맑은 고딕", Font.PLAIN, 18);
 		RoundJTextField textField = new RoundJTextField(30);
 		textField.setFont(font);
-		textField.setBounds(95,70,250,70);
-		textField.setForeground(new Color(180, 180, 180));
+		textField.setBounds(76,60,260,65);
+		textField.setForeground(new Color(160, 160, 160));
+		textField.setText("전화번호를 입력하세요");  
 		
 		//번호 버튼이 들어갈 패널 추가
 		JPanel num_panel = new JPanel();
 		num_panel.setPreferredSize(new Dimension(350,400));
-		num_panel.setBounds(70,170,300,330);
+		num_panel.setBounds(55,160,300,330);
 		num_panel.setLayout(new GridLayout(4,3,8,8));
 		num_panel.setBackground(new Color(245, 235, 208));
 		
-		//번호 버튼 생성
+	
 		ArrayList<JButton> numBtns = new ArrayList<>();
+		StringBuilder phoneNum = new StringBuilder();  
+		//텍스트 필드에서 입력 받는 숫자를 이어서 받기 위해 스트링빌더 준비
+		
+		
+		//(액션)======================================================================
+		
+		//텍스트필드액션 불러오기
+		textField.addMouseListener(new AccumulatedFundTextFiel(textField));
+		//텍스트필드 비활성화 시켜서 키보드로 입력 못하게 막음.
+		textField.setEditable(false); 
 		
 		//numBtns의 인덱스 9,10,11은 ←,0,적립으로 글자 변경 or 각각의 맞는 스타일로 다시 변경.
 		for (int i = 0; i < 12; i++) {
@@ -66,28 +82,28 @@ public class AccumulatedFundFrame extends DefaultFrame{
 				numBtns.add(new JButton("←"));
 				num_button_style(numBtns,i);
 				numBtns.get(i).setFont(new Font("궁서체",Font.BOLD,30));
-				
 			}else if (i == 11) {
 				numBtns.add(new JButton("적립"));
 				num_button_style(numBtns,i);
 				numBtns.get(i).setBackground(new Color(115,185,112));
 				numBtns.get(i).setForeground(Color.white);  //버튼 글자색 변경
-				
 			}else {
 				numBtns.add(new JButton(Integer.toString(i + 1)));
 				num_button_style(numBtns,i);
 			}
-			numBtns.get(i).addMouseListener(new AccumulatedButton(textField, numBtns));
+			
+			//버튼 클릭시 일어나는 액션
+			numBtns.get(i).addMouseListener(new AccumulatedFundButton(this,textField, numBtns, phoneNum));
+		
 		}
 		
+		
+		//(프레임에 추가하면서 마무리)========================================================
+		
+		//번호패널에 번호 버튼들 추가
 		for (JButton btn : numBtns) {
 			num_panel.add(btn);	
 		}
-		
-		textField.setText("전화번호를 입력하세요");  
-	
-		//텍스트필드액션 불러오기
-		textField.addMouseListener(new AccumulatedTextFiel(textField));
 		
 		//프레임에 패널이랑 텍스트필드 추가
 		add(num_panel);
@@ -98,12 +114,16 @@ public class AccumulatedFundFrame extends DefaultFrame{
 	}
 	
 	//버튼 스타일 메서드
-	void num_button_style(ArrayList<JButton> numBtns, int num) {
+	private void num_button_style(ArrayList<JButton> numBtns, int num) {
 		numBtns.get(num).setFont(new Font("맑은 고딕",Font.PLAIN,20));
 		numBtns.get(num).setBackground(new Color(255,255,255));
 		numBtns.get(num).setForeground(Color.GRAY);
 		numBtns.get(num).setBorderPainted(false);
 		
+	}
+
+	public JFrame getAccumulatedFundFrame() {
+		return this;
 	}
 	
 	public static void main(String[] args) {
