69a1662bf73283525d784d583106f4d4437f8c3b
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:51:35 +0900

    결제완료를 누르면 결제 날짜와 총 금액이 오더테이블로 입력되는 메서드 구현

---
+++ Cafe-POS-System/src/jdbc/method/InsertOrderTable.java
@@ -0,0 +1,31 @@
+package jdbc.method;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JTable;
+
+import jdbc.hikari.HikariCP;
+
+public class InsertOrderTable {
+	private String sql = "INSERT INTO order_table (order_id, order_time, order_total) "
+			+ "VALUES (order_id_seq, TO_CHAR(sysdate, 'YYYY-MM-DD'), ?)";
+	private int totalPrice;
+	public InsertOrderTable(int totalPrice) {
+		this.totalPrice = totalPrice;
+		try (
+				Connection conn = HikariCP.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				) {
+			pstmt.setInt(1, totalPrice);
+			ResultSet rs = pstmt.executeQuery();
+			
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
