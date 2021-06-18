dc5b2179453942597efa42019ea9684c194c30fc
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:36:00 +0900

    영수증 정보를 문자열으로 받아줄 텍스트아레아 구현

---
+++ Cafe-POS-System/src/jdbc/method/ReceiptTextArea.java
@@ -0,0 +1,17 @@
+package jdbc.method;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.TextArea;
+
+public class ReceiptTextArea extends TextArea {
+	
+	public ReceiptTextArea(String html) {
+		setText(html);
+		setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		setLocation(0, 0);
+		setSize(660, 950);
+		setBackground(Color.WHITE);
+		new ReceiptJScrollPane().add(this);
+	}
+}
