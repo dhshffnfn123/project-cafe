74e31a3155548f99e11ce24a58afeab47d7fbf0d
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:39:30 +0900

    영수증 정보에 결제테이블 안에 있는 메뉴 정보를 가져오는 메서드 구현

---
+++ Cafe-POS-System/src/jdbc/method/GetMenuInfo.java
@@ -0,0 +1,34 @@
+package jdbc.method;
+
+import java.util.ArrayList;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class GetMenuInfo {
+	private JTable table;
+	private DefaultTableModel model;
+	private int index;
+	private int sum;
+	private StringBuilder str_menu = new StringBuilder();
+	public GetMenuInfo(JTable table) {
+		this.table = table;
+		model = (DefaultTableModel)table.getModel();
+		index = 0;
+		for (int i = 0; i < model.getRowCount(); i++) {
+			int totalPrice = Integer.parseInt((String) model.getValueAt(index, 3));
+			int totalCount = Integer.parseInt((String) model.getValueAt(index, 2));
+			str_menu.append(String.format("%-31s\n", model.getValueAt(index, 1)));
+			str_menu.append(String.format("\t\t\t\t\t%5d\t\t%4d\t\t%11d\n", totalPrice / totalCount, totalCount, totalPrice));
+			sum += totalPrice;
+			index++;								
+		}
+	}
+	public StringBuilder getMenuInfo() {
+		return str_menu;
+	}
+	public int getSum() {
+		return sum;
+	}
+	
+}
