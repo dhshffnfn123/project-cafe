2d3caed4879c4fe09d0af78138bf8789a7473287
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:38:43 +0900

    영수증 정보에 결제자(근무자)를 로그인 Frame에서 가져오는 메서드 구현

--- Cafe-POS-System/src/jdbc/method/RoadEmployeeName.java
+++ Cafe-POS-System/src/jdbc/method/RoadEmployeeName.java
@@ -8,20 +8,12 @@ import java.sql.SQLException;
 import jdbc.hikari.HikariCP;
 
 public class RoadEmployeeName {
-	private String sql = "SELECT employee_name FROM employees_table INNER JOIN order_table USING(employee_id)";
 	private String employeeName;
 	public RoadEmployeeName() {
-		try (
-				Connection conn = HikariCP.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, 
-			             ResultSet.CONCUR_UPDATABLE);
-				ResultSet rs = pstmt.executeQuery();
-				) {
-			rs.last();
-			this.employeeName = rs.getString(1);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	
+	}
+	public RoadEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
 	}
 	public String getEmployeeName() {
 		return employeeName;
