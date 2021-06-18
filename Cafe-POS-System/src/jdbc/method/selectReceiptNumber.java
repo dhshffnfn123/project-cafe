c90b367f1ede9e81894c52999478e0598a5d7731
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:38:15 +0900

    영수증 정보에 영수증 번호를 가져오는 메서드 구현

---
+++ Cafe-POS-System/src/jdbc/method/selectReceiptNumber.java
@@ -0,0 +1,30 @@
+package jdbc.method;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jdbc.hikari.HikariCP;
+
+public class selectReceiptNumber {
+	private String sql = "SELECT receipt_id FROM receipt_table ORDER BY receipt_id";
+	private int receiptNumber;
+	public selectReceiptNumber() {
+		try (
+				Connection conn = HikariCP.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, 
+			             ResultSet.CONCUR_UPDATABLE);
+				ResultSet rs = pstmt.executeQuery();
+
+		){
+			rs.last();
+			receiptNumber = rs.getInt(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public int getReceiptNumber() {
+		return receiptNumber;
+	}
+}
