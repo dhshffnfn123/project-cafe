1d5aa780705be2fc1881a350cba9751909e8b265
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-18 12:36:25 +0900

    텍스트 아레아를 위 아래로 스크롤할 수 있는 스크롤팬 구현

---
+++ Cafe-POS-System/src/jdbc/method/ReceiptJScrollPane.java
@@ -0,0 +1,12 @@
+package jdbc.method;
+
+import java.awt.Dimension;
+
+import javax.swing.JScrollPane;
+
+public class ReceiptJScrollPane extends JScrollPane{
+	
+	public ReceiptJScrollPane() {
+		setPreferredSize(new Dimension(660, 1000));
+	}
+}
