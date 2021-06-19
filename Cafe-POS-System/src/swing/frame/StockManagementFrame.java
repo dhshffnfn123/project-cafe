81427e1f4cd3de9f5dc4ba259b356488b3b9321d
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-20 00:22:10 +0900

    직원 관리 창에 직급을 건내주기위해 String grade를 선언 후 직급을 보내주는 메서드 추가

--- Cafe-POS-System/src/swing/frame/StockManagementFrame.java
+++ Cafe-POS-System/src/swing/frame/StockManagementFrame.java
@@ -38,13 +38,13 @@ public class StockManagementFrame extends DefaultFrame {
 	private JLabel title_label;
 	private JButton add_btn, update_btn, delete_btn, null_btn;
 	private DefaultTableCellRenderer dtcr_center, dtcr_right;
-
+	private String grade;
 	Font bigger_font = new Font("¸¼Àº °íµñ", Font.BOLD, 40);
 	Font big_font = new Font("¸¼Àº °íµñ", Font.BOLD, 30);
 	Font nomal_font = new Font("¸¼Àº °íµñ", Font.PLAIN, 15);
 	Font small_font = new Font("¸¼Àº °íµñ", Font.BOLD, 15);
 
-	public StockManagementFrame() {
+	public StockManagementFrame(String grade) {
 		setLayout(new BorderLayout());
 		setTitle("Stock Management");
 
@@ -60,7 +60,7 @@ public class StockManagementFrame extends DefaultFrame {
 		back_btn.setBorderPainted(false);
 		topPanel.add(back_btn, BorderLayout.WEST);
 		back_btn.setHorizontalAlignment(SwingConstants.LEFT);
-		back_btn.addMouseListener(new BackButtonMouseAction(this));
+		back_btn.addMouseListener(new BackButtonMouseAction(this, grade));
 
 		// °¡¿îµ¥ ½Ã½ºÅÛ½Ã°è
 		JLabel clock = new CurrentTimeClock().setClock();
