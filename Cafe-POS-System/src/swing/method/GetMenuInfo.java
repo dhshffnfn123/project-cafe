c60898cd3c74ac1c2945b05aa961dabd1d5ded38
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-06-20 00:49:08 +0900

    영수증에 있는 메뉴들을 재고관리에 보내주기 위한 메서드들을 추가

--- Cafe-POS-System/src/swing/method/GetMenuInfo.java
+++ Cafe-POS-System/src/swing/method/GetMenuInfo.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import jdbc.method.RoadProductInfo;
+import jdbc.method.RoadRtdInfo;
+import jdbc.method.RoadStockCount;
+import swing.view.ProductView;
+
 public class GetMenuInfo {
 	private JTable table;
 	private DefaultTableModel model;
@@ -19,20 +24,21 @@ public class GetMenuInfo {
 		index = 0;
 		
 		for (int i = 0; i < model.getRowCount(); i++) {
-			int totalCount = (int)model.getValueAt(index, 2);
-			int totalPrice = (int)model.getValueAt(index, 3);
+			int total_count = (int)model.getValueAt(index, 2);
+			int total_price = (int)model.getValueAt(index, 3);
+			String menu_name = (String) model.getValueAt(index, 1);
+			str_menu.append(String.format("%-31s\n", menu_name));
+			str_menu.append(String.format("\t\t\t\t\t%5d\t\t%4d\t\t%11d\n", total_price / total_count, total_count, total_price));
 
-			str_menu.append(String.format("%-31s\n", model.getValueAt(index, 1)));
-			str_menu.append(String.format("\t\t\t\t\t%5d\t\t%4d\t\t%11d\n", totalPrice / totalCount, totalCount, totalPrice));
-			sum += totalPrice;
-			index++;								
+			sum += total_price;
+			index++;
+			new RoadProductInfo(menu_name, total_count);
+			new RoadRtdInfo(menu_name, total_count);
 		}
 	}
-	
 	public StringBuilder getMenuInfo() {
 		return str_menu;
 	}
-	
 	public int getSum() {
 		return sum;
 	}
