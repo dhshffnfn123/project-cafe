b39ed4b90d5d9aab1f67c71503a8d6424c4462cc
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-20 00:36:46 +0900

    색깔 바뀌는 현상 해결

--- Cafe-POS-System/src/swing/frame/ReceiptDefaultFrame.java
+++ Cafe-POS-System/src/swing/frame/ReceiptDefaultFrame.java
@@ -7,11 +7,12 @@ import swing.method.ReceiptJScrollPane;
 import swing.method.ReceiptLabel;
 
 public class ReceiptDefaultFrame extends DefaultFrame {
+	
 	public ReceiptDefaultFrame() {
 		setTitle("Receipt");
 		new ReceiptJScrollPane();
 		setLayout(null);
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setSize(650, 1000);
 		setLocation(1000, 250);
 		setResizable(false);
--- Cafe-POS-System/src/swing/method/ReceiptJScrollPane.java
+++ Cafe-POS-System/src/swing/method/ReceiptJScrollPane.java
@@ -4,7 +4,7 @@ import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
 
-public class ReceiptJScrollPane extends JScrollPane{
+public class ReceiptJScrollPane extends JScrollPane {
 	
 	public ReceiptJScrollPane() {
 		setPreferredSize(new Dimension(660, 1000));
