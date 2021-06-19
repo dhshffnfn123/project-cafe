f9428aaf5c49f138dd5fa8b76acf635a0990f9fa
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-19 17:03:46 +0900

    뒤로가기 버튼 액션 생성자 오버라이딩, 주문 시 뒤로가기 할 때 테이블 다시 초기화

--- Cafe-POS-System/src/action/BackButtonMouseAction.java
+++ Cafe-POS-System/src/action/BackButtonMouseAction.java
@@ -4,34 +4,56 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import swing.frame.ChoosePageFrame;
+import swing.frame.OrderFrame;
 
 public class BackButtonMouseAction implements MouseListener {
 
 	private JFrame frame;
-	
+	private JTable table;
+	private DefaultTableModel model;
+
 	public BackButtonMouseAction(JFrame frame) {
 		this.frame = frame;
 	}
-	
+
+	public BackButtonMouseAction(JFrame frame, JTable table) {
+		this.frame = frame;
+		this.table = table;
+		this.model = (DefaultTableModel)table.getModel();
+	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
+		model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
+
+		OrderFrame.GettableInfo().removeAll(OrderFrame.GettableInfo());
+		OrderFrame.GetMenuHash().clear();
+		OrderFrame.getTotalmoney().setText(String.valueOf(0));
+
 		frame.dispose();
-		
+
 		new ChoosePageFrame();
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {
+	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {
+	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {
+	}
 
 }
